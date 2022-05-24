6d93311bda321da7c3e58792319bde251c55e2a2
T-sugo <taba.takashi.s@gmail.com>
2022-05-24 19:37:01 +0900

    追加；reactとtypescriptとlaravelui追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.ts('resources/ts/app.tsx', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()){
+    mix.version();
+}
